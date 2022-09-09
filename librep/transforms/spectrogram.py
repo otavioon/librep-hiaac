bc4c3c2ed58abdf8f7c487e3a15449c58b2bc2e4
Otavio Napoli <otavio.napoli@gmail.com>
2022-09-09 16:45:12 +0000

    Fix #8

--- librep/transforms/spectrogram.py
+++ librep/transforms/spectrogram.py
@@ -14,6 +14,7 @@ class Spectrogram(Transform):
         self.fs = fs
         self.segment_size = segment_size
         self.overlap = overlap
+
     # TODO
     def transform(self, X: ArrayLike):
         Sxx, f, t = signal.spectrogram(X,
